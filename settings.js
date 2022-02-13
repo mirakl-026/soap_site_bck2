5541bb394b05cd073e5d6ff4036dae5476845671
Evgeniy Bushin <evgeniy.pgm@inbox.ru>
2022-02-13 16:56:44 +0300

    удалил keys, настройки вынес в settings.js, настройки почты в не-гитовый mail/mailSettings.js

--- settings.js
+++ settings.js
@@ -1,3 +1,8 @@
 module.exports = {
-    PROJECT_DIR: __dirname
+    NEXT_PUBLIC_PORT:3000,
+    NEXT_PUBLIC_DOMAIN: "http://localhost:3000",
+
+    PROJECT_DIR: __dirname,
+
+    MONGODB_URI: "mongodb://127.0.0.1:27017/soap_hbs_v2",
 }
