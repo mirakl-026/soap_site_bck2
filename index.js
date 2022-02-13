8dd4fac82744b6a8d9b07e1435388dc8db895557
Evgeniy Bushin <evgeniy.pgm@inbox.ru>
2022-02-13 19:40:10 +0300

    добавил пример JWT - модели user, role, middleware, controller, router

--- index.js
+++ index.js
@@ -2,18 +2,10 @@ const express = require("express");
 const exphbs = require("express-handlebars");
 const mongoose = require("mongoose");
 const path = require("path");
-const keys = require("./keys/keys");
-
-const session = require("express-session");
-const MongoStore = require("connect-mongodb-session")(session);
-const varMiddleware = require("./middleware/variables");
-const csurf = require("csurf");
 const compression = require('compression')
 
 // TODO доделать все API
-
 // роуты API
-const accountRoutes = require("./routes/api/accountRouter");
 const collectionRoutes = require("./routes/api/collectionRouter");
 const orderRoutes = require("./routes/api/orderRouter");
 const productRoutes = require("./routes/api/productRouter");
@@ -28,15 +20,12 @@ const searchRouter = require("./routes/api/searchRouter");
 // роуты страниц и админа
 const authRoutes = require("./routes/authRouter");
 const imageRoutes = require("./routes/api/imagesRouter");
-
-const homeRoutes = require("./routes/homeRouter");
-const admin_panelRoutes = require("./routes/admin/adminPanelRouter");
-
+const homeRoutes = require("./routes/pages/homeRouter");
+const adminRoutes = require("./routes/pages/adminPanelRouter");
 // сервис изначальной инициализации
 const backInit = require("./init");
 
-
-
+const settings = require("./settings");
 
 
 
@@ -56,33 +45,14 @@ app.set("views", "views");
 
 // регистрация папки public как статической
 app.use(express.static(path.join(__dirname, "public")));
+
 // позволяет декодировать http запросы и получать из body. элементы
 app.use(express.urlencoded({extended: true}));
 
-// подключение сессии
-// объект mongo
-const store =  new MongoStore({
-    collection: "sessions",
-    uri: keys.MONGODB_URI
-});
-
-app.use(session({
-    secret: keys.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false,
-    store: store
-}));
-app.use("/api/image", imageRoutes);
-app.use(csurf()); // *
-app.use(varMiddleware);
-
 // подключаем роуты в конвейер
-app.use("/admin/", admin_panelRoutes);
 app.use("/auth", authRoutes);
 app.use("/backup", backupRouter);
-
-
-app.use("/api/account", accountRoutes);
+app.use("/api/image", imageRoutes);
 app.use("/api/collection", collectionRoutes);
 app.use("/api/order", orderRoutes);
 app.use("/api/product", productRoutes);
@@ -93,15 +63,16 @@ app.use("/api/sale", saleRoutes);
 app.use("/api/deletedEntity",deletedRoutes);
 app.use("/api/search", searchRouter);
 
+// страницы hbs
+app.use("/", adminRoutes);
 app.use("/", homeRoutes);
 
 
 // порт и запуск сервера
-const PORT = process.env.PORT || 3000;
 // запуск express + mongoose
 async function start() {
     try {
-        await mongoose.connect(keys.MONGODB_URI, {
+        await mongoose.connect(settings.MONGODB_URI, {
             useNewUrlParser: true,
             // useFindAndModify: false
         });
@@ -109,8 +80,8 @@ async function start() {
         // инициализация всего
         await backInit.init();
 
-        app.listen(PORT, () => {
-            console.log(`Server running on port ${PORT}...`);
+        app.listen(settings.NEXT_PUBLIC_PORT, () => {
+            console.log(`Server running on port ${settings.NEXT_PUBLIC_PORT}...`);
         });
 
     } catch (error) {
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -7,3 +7,32 @@ const router = Router();
 
 
 module.exports = router;
+
+/*
+const express = require("express");
+const authController = require("../controllers/authController");
+const {check} = require("express-validator");
+
+const authMiddleware = require("../middleware/authMiddleware");
+const roleMiddleware = require("../middleware/roleMiddleware");
+
+const router = express.Router();
+
+router.post("/register", [
+    check("username", "нужно имя").notEmpty(),
+    check("password", "пароль от 3 до 10 символов").isLength({min: 3, max: 10})
+], authController.register);
+
+router.post("/login", authController.login);
+
+// router.get("/users", authMiddleware, authController.getUsers);
+router.get("/users", roleMiddleware(["ADMIN"]), authController.getUsers);
+
+
+router.get("/init", authController.initRoles);
+
+router.post("/initAdmin", authController.initAdmin);
+
+module.exports = router;
+
+ */
