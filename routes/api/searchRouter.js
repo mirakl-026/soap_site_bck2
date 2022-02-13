c78f0bf5f1ae6d6a46439f155763d3aa11ee6da1
Evgeniy Bushin <evgeniy.pgm@inbox.ru>
2022-02-13 18:44:00 +0300

    разбил роутер поиска - на роутер и контроллер

--- routes/api/searchRouter.js
+++ routes/api/searchRouter.js
@@ -1,49 +1,17 @@
 const express = require("express");
-const SearchService = require("../../services/mongodb/searchService");
-const adm_auth = require("../../middleware/checkAdmMW");
+const searchController = require("../../controllers/api/searchController");
+
 
 const router = express.Router();
 
-router.get("/find", async (req, res) => {
-    try {
-        let userInput = req.query.userInput;
-        const foundedItems = await SearchService.find(userInput);
-        res.status(200).json(SearchService.searchResultToJSON(foundedItems));
+//
+router.get("/find", searchController.find);
 
-    } catch (e) {
-        res.status(500).json({
-            message: "server error:" + e.message
-        });
-    }
-});
-
-
-router.get("/findInNames", async (req, res) => {
-    try {
-        let userInput = req.query.userInput;
-        const foundedItems = await SearchService.findInNames(userInput);
-        res.status(200).json(SearchService.searchResultToJSON(foundedItems));
-
-    } catch (e) {
-        res.status(500).json({
-            message: "server error:" + e.message
-        });
-    }
-});
-
+//
+router.get("/findInNames", searchController.findInNames);
 
 // явное обновление поисковой информации
-router.post("/update", adm_auth, async (req, res) => {
-    try {
-        await SearchService.refreshSearchData();
-        res.status(200).json({status: "all search data updated"});
-
-    } catch (e) {
-        res.status(500).json({
-            message: "server error:" + e.message
-        });
-    }
-});
+router.post("/update", searchController.update);
 
 
 module.exports = router;
